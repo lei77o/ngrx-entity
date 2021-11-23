bdd54e0ba5a090f30ba0de7933a69de622975240
lei77o <albrecht.leo@gmail.com>
2021-11-22 23:17:33 -0300

    bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PizzaOrderComponent } from './component/pizza/pizza-order/pizza-order.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './component/reducers'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment.prod';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
